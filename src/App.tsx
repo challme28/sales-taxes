116735170ce1aa1607b4b0d33c578cd4eaa9026d
Christian Llanos <challme28@gmail.com>
2022-02-19 18:29:23 -0300

    Fix instructions style and sort css

--- src/App.tsx
+++ src/App.tsx
@@ -28,15 +28,14 @@ function App() {
     <main className="App">
       <section className="instructions">
         <h3>Please enter your shopping basket with the following format:</h3>
-        <p>1 Book at 12.49</p>
-        <p>(quantity) (name) at (price)</p>
-        <p>1 Imported bottle of perfume at 47.50</p>
-        <p>(quantity) (name) at (price)</p>
+        <p className="product-line">1 Book at 12.49</p>
+        <p className="product-example">(quantity) (name) at (price)</p>
+        <p className="product-line">1 Imported bottle of perfume at 47.50</p>
+        <p className="product-example">(quantity) (name) at (price)</p>
       </section>
       <textarea
         className="products-list"
-        cols={60}
-        rows={20}
+        rows={15}
         onChange={handleChange}
         value={list}
       />
